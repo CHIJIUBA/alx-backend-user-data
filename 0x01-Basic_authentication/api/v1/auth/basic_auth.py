971ae67c5514913f318e81498e627f0c745016cb
Chijiuba Victory <chijiubaonyedikachukwu@gmail.com>
2023-03-09 18:56:28 +0100

    go

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -28,7 +28,7 @@ class BasicAuth(Auth):
                 authorization_header.split()[0].lower() != "basic":
             return None
         return authorization_header.split(" ")[1]
-    
+
     def decode_base64_authorization_header(
             self, base64_authorization_header: str) -> str:
         """
