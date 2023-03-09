54f7323f5cc7008766450088641c2e13e9005f83
Chijiuba Victory <chijiubaonyedikachukwu@gmail.com>
2023-03-09 18:50:21 +0100

    go

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,7 @@
 """ Basic Auth Module
 """
 from flask import request
+import base64
 from typing import List, TypeVar
 from api.v1.auth.auth import Auth
 
@@ -27,3 +28,25 @@ class BasicAuth(Auth):
                 authorization_header.split()[0].lower() != "basic":
             return None
         return authorization_header.split(" ")[1]
+    
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """
+        Decodes the base64 encoded string
+        Args:
+            base64_authorization_header: base64 encoded string
+        Returns:
+            decoded base64 encoded string
+        Raises:
+            Exception: if base64_authorization_header
+            is not a valid base64 encoded string
+        """
+        if not base64_authorization_header or \
+                type(base64_authorization_header) is not str:
+            return None
+        try:
+            encode_string = base64_authorization_header.encode("utf-8")
+            decoded_str = base64.b64decode(encode_string)
+            return decoded_str.decode("utf-8")
+        except Exception:
+            return None
