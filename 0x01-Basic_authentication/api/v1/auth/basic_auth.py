8506ce1ebb9857ad6469ab42057d09b640727695
Chijiuba Victory <chijiubaonyedikachukwu@gmail.com>
2023-03-09 18:36:55 +0100

    go

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" Basic Auth Module
+"""
+from flask import request
+from typing import List, TypeVar
+from auth import Auth
+
+
+class BasicAuth(Auth):
+    """Auth Module Class
+    """
+    pass
