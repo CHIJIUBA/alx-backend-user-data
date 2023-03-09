bd747346327b62eee48c372b2c8da6d848e60b0e
Chijiuba Victory <chijiubaonyedikachukwu@gmail.com>
2023-03-09 18:47:30 +0100

    go

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -9,4 +9,21 @@ from api.v1.auth.auth import Auth
 class BasicAuth(Auth):
     """Auth Module Class
     """
-    pass
+    def extract_base64_authorization_header(
+            self, authorization_header: str) -> str:
+        """
+        Extracts the base64 encoded string from the
+        authorization header
+        Args:
+            authorization_header: authorization header
+        Returns:
+            base64 encoded string
+        Raises:
+            ValueError: if authorization_header
+            is not a valid string
+        """
+        if not authorization_header or \
+                type(authorization_header) is not str or \
+                authorization_header.split()[0].lower() != "basic":
+            return None
+        return authorization_header.split(" ")[1]
