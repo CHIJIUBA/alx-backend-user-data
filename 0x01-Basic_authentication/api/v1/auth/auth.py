9383aad0cbffe688e25e666f37d1283fa824bf32
Chijiuba Victory <chijiubaonyedikachukwu@gmail.com>
2023-03-09 18:02:19 +0100

    go

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -12,6 +12,17 @@ class Auth():
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ requires the given auth
         """
+        if path is None or excluded_paths is None or excluded_paths == []:
+            return True
+        if path[-1] != '/':
+            path += '/'
+        for excluded_path in excluded_paths:
+            if excluded_path.endswith('*'):
+                if path.startswith(excluded_path[:-1]):
+                    return False
+        if path not in excluded_paths:
+            return True
+
         return False
 
     def authorization_header(self, request=None) -> str:
