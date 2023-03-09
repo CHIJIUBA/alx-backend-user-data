76dd150c2c9bed16096a6d175f087299d002d4b8
Chijiuba Victory <chijiubaonyedikachukwu@gmail.com>
2023-03-09 13:48:27 +0100

    go

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+""" Auth Module
+"""
+from flask import request
+
+
+class Auth():
+    """Auth Module
+    """
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        return False
+    
+    
+    def authorization_header(self, request=None) -> str:
+        return None
+    
+    
+    def current_user(self, request=None) -> TypeVar('User'):
+        return None
